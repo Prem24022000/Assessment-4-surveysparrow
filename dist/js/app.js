3b8470ed6075a5afe01e731b6ee3195d685318a4
Prem Prakash Sahu <69319430+Prem24022000@users.noreply.github.com>
2023-02-02 16:53:15 +0530

    Initially deployment

---
+++ dist/js/app.js
@@ -0,0 +1,33 @@
+const lines = document.querySelectorAll('.scroll-indicator a');
+
+const removeActiveClass = () => {
+  lines.forEach((line) => {
+    line.classList.remove('active');
+  });
+};
+
+const addActiveClass = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      let currentLine = document.querySelector(
+        `.scroll-indicator a[href='#${entry.target.id}']`
+      );
+      removeActiveClass();
+      currentLine.classList.add('active');
+    }
+  });
+};
+
+const option = {
+  threshold: 0.6,
+};
+
+const sections = document.querySelectorAll('section');
+
+const observer = new IntersectionObserver(addActiveClass, option);
+
+// console.log(section);
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
